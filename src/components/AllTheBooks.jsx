cf73d9be2ae8c540d8af52e81b124ad12a295287
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-03-12 17:41:03 +0100

    minor fixes

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -2,7 +2,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import { Component } from "react";
 import books from "../data/books/scifi.json";
 
@@ -13,12 +12,15 @@ class AllTheBooks extends Component {
         <Row>
           {books.map((book, i) => {
             return (
-              <Col xs={6} md={4} lg={2}>
+              <Col xs={6} md={4} lg={2} key={book.asin}>
                 <Card className="my-3">
                   <Card.Img variant="top" src={book.img} />
                   <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <Card.Text>{book.category}</Card.Text>
+                    <Card.Title className="mb-3">{book.title}</Card.Title>
+                    <div className="d-flex">
+                      <Card.Text className="me-auto">{book.category}</Card.Text>
+                      <Card.Text>{book.price}€</Card.Text>
+                    </div>
                   </Card.Body>
                 </Card>
               </Col>
